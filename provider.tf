210b3ad8bb208079bb708fc01f2edd95abead4fc
Nikita <lnikita2410@gmail.com>
2023-05-11 12:03:03 +0530

    added backend

--- provider.tf
+++ provider.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "tf-backendfiles"
+    key    = "path/to/my/key"
+    region = "us-west-1"
+  }
   
 }
 
 provider "aws" {
   region = "us-west-1"
-  access_key = ""
-  secret_key = ""
 }
