5a46db6c35586e61a551afab1ec4d9eb00e7fb0a
Nikita <lnikita2410@gmail.com>
2023-05-11 14:57:40 +0530

    added keypair code

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-# resource "aws_key_pair" "auth-key" {
-#   key_name = "auth-key"
-#   public_key = tls_private_key.rsa.public_key_openssh
-# }
-
-
-# resource "tls_private_key" "rsa" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
-
-resource "aws_instance" "demo-vm" {
-  # depends_on = [ aws_key_pair.auth-key ]
-  ami= "ami-051ed863837a0b1b6"
-  instance_type = "t2.micro"
-  # key_name = aws_key_pair.auth-key
+resource "aws_key_pair" "auth-key" {
+  key_name = "auth-key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
-# resource "aws_security_group_rule" "ssh-allow" {
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 22
-#   protocol          = "tcp"
-# }
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_instance" "demo-vm" {
+   depends_on = [ aws_key_pair.auth-key ]
+  ami= "ami-051ed863837a0b1b6"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.auth-key
+}
+
+resource "aws_security_group_rule" "ssh-allow" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 22
+  protocol          = "tcp"
+}
