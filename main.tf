4a865a6681569070013a9c8a1964ddc350a2809e
nikita230 <69594449+nikita230@users.noreply.github.com>
2023-05-11 10:47:08 +0530

    Update main.tf
    undo

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#resource "aws_instance" "demo-vm" {
- # ami= "ami-051ed863837a0b1b6"
-  #instance_type = "t2.micro"
-#}
+resource "aws_instance" "demo-vm" {
+  ami= "ami-051ed863837a0b1b6"
+  instance_type = "t2.micro"
+}
