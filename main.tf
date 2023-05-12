b2836abc1dc98caf5eb34344929cbe286d30834c
Nikita <lnikita2410@gmail.com>
2023-05-12 13:07:28 +0530

    added user-data:

--- main.tf
+++ main.tf
@@ -15,30 +15,30 @@ resource "aws_instance" "demo-vm" {
   instance_type = "t2.micro"
   key_name = "auth-key"
 
- connection {
-    type     = "ssh"
-    user     = "root"
-    host     = self.public_ip
-   #private_key = aws_key_pair.auth-key
-   host_key = "aws_key_pair.auth-key"
-  }
+#  connection {
+#     type     = "ssh"
+#     user     = "root"
+#     host     = self.public_ip
+#    #private_key = aws_key_pair.auth-key
+#    host_key = "aws_key_pair.auth-key"
+#   }
 
  #security_groups = ["aws_security_group.allow_ssh"
 
- provisioner "remote-exec" {
- inline = [ 
-  "sudo yum update –y",
-  "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-  "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key"  ,
-  "sudo yum upgrade",
-  "sudo amazon-linux-extras install java-openjdk11 -y",
-  "sudo dnf install java-11-amazon-corretto -y",
-  "sudo yum install jenkins -y",
-  "sudo systemctl enable jenkins",
-  "sudo systemctl start jenkins",
-  "sudo systemctl status jenkins"
-  ]
- }
+user_data = <<-EOF
+  #!bin/bash
+  sudo yum update –y
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+  sudo yum upgrade
+  sudo amazon-linux-extras install java-openjdk11 -y
+  sudo dnf install java-11-amazon-corretto -y
+  sudo yum install jenkins -y
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  sudo systemctl status jenkins
+  EOF
+
 }
 
 resource "aws_security_group" "allow_ssh" {
