0c026bdeef6eb3dd0f4f067955b4744be5589e3e
Nikita <lnikita2410@gmail.com>
2023-05-12 12:30:21 +0530

    Added remote exec

--- main.tf
+++ main.tf
@@ -14,6 +14,23 @@ resource "aws_instance" "demo-vm" {
   ami= "ami-051ed863837a0b1b6"
   instance_type = "t2.micro"
   key_name = "auth-key"
+ security_groups = ["aws_security_group.allow_ssh"]
+
+ provisioner "remote-exec" {
+ inline = [ 
+  "sudo yum update –y",
+  "sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+  "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key"  ,
+  "sudo yum upgrade",
+  "sudo amazon-linux-extras install java-openjdk11 -y",
+  "sudo dnf install java-11-amazon-corretto -y",
+  "sudo yum install jenkins -y",
+  "sudo systemctl enable jenkins",
+  "sudo systemctl start jenkins",
+  "sudo systemctl status jenkins"
+  ]
+ }
 }
 
 resource "aws_security_group" "allow_ssh" {
